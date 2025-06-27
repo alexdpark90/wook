753f75e61bdd865a088e0e094d70e7b56af8c1be
alexdpark90 <97861690+alexdpark90@users.noreply.github.com>
2025-06-27 17:21:44 +0900

    up 0627

--- js/app.js
+++ js/app.js
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded" , function(){
     const searchInput = document.getElementById("searchInput");
     const portfolioitems = document.querySelectorAll(".portfolio-item");
 
-    searchInput.addEventListener("input", function(){
-        const searchTerm = this.value.toLowerCase();
-        portfolioitems.forEach((item) => {
-            const title = item.querySelector("h4").textContent.toLowerCase();
-            if(title.includes(searchTerm)) {
-                item.style.display = "block";
-            } else {
-                item.style.display = "none";
-            }
-        })
+    // searchInput.addEventListener("input", function(){
+    //     const searchTerm = this.value.toLowerCase();
+    //     portfolioitems.forEach((item) => {
+    //         const title = item.querySelector("h4").textContent.toLowerCase();
+    //         if(title.includes(searchTerm)) {
+    //             item.style.display = "block";
+    //         } else {
+    //             item.style.display = "none";
+    //         }
+    //     })
 
-    })
+    // })
 
 
     const filterButtons = document.querySelectorAll(".filter-btn")
